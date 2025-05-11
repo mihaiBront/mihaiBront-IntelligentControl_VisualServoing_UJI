003306eb913f51caee88c8932504dea29ed4ee71
MihaiAlienware <brontmihai1986@gmail.com>
2025-05-11 11:45:23 +0200

    Minor changes and  gitignore

--- scripts/DATASET_GenerateFullDataSet.bash
+++ scripts/DATASET_GenerateFullDataSet.bash
@@ -2,6 +2,6 @@
 
 python generateDataset.py \
     --num-sequences 20000 \
-    --output-dir dataset_sample \
+    --output-dir training_generation \
     --max-iterations 500 \
     --savebunch-size 500
